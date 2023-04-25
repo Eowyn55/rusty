7501bae83cbaff62d5bddb950780d457f04a7c8a
Milica Stojiljkovic <milica.stojiljkovic@zuhlke.com>
2023-04-25 15:50:22 +0100

    Small basic rust server.

---
+++ rest_server/target/debug/rest_server.d
@@ -0,0 +1,1 @@
+C:\Users\stom\Documents\Learning\RUST\Demo\ projects\rusty\rest_server\target\debug\rest_server.exe: C:\Users\stom\Documents\Learning\RUST\Demo\ projects\rusty\rest_server\src\main.rs
