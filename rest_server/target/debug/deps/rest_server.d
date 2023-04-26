edaf901ef312eaf04624c0a55d43f3a7b7d24a89
Milica Stojiljkovic <milica.stojiljkovic@zuhlke.com>
2023-04-26 11:54:20 +0100

    Simple server refactorings.

--- rest_server/target/debug/deps/rest_server.d
+++
@@ -1,5 +0,0 @@
-C:\Users\stom\Documents\Learning\RUST\Demo projects\rusty\rest_server\target\debug\deps\rest_server.exe: src\main.rs
-
-C:\Users\stom\Documents\Learning\RUST\Demo projects\rusty\rest_server\target\debug\deps\rest_server.d: src\main.rs
-
-src\main.rs:
--- rest_server/target/debug/rest_server.d
+++
@@ -1,1 +0,0 @@
-C:\Users\stom\Documents\Learning\RUST\Demo\ projects\rusty\rest_server\target\debug\rest_server.exe: C:\Users\stom\Documents\Learning\RUST\Demo\ projects\rusty\rest_server\src\main.rs
